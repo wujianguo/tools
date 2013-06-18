193912c37d4b5ffcd537d73bb2c8ae0bc649ead3
wujianguo <lsjustin89@gmail.com>
2013-06-18 20:27:03 +0800

    player

--- network/player.py
+++ network/player.py
@@ -1,57 +1,171 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from BaseHTTPServer import HTTPServer,BaseHTTPRequestHandler
 import SocketServer
-import logging
-import threading
 import os
-import re
+import posixpath
+import BaseHTTPServer
+import urllib
+import cgi
 import shutil
-ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-HOST = '127.0.0.1'
-PORT = 9854
-BUF_SIZE = 256*1024
-class StreamHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        logging.error(self.headers)
-        file_path = os.path.join(ROOT_DIR, self.path[1:])
+import mimetypes
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 
-        f = self.send_head(file_path)
+
+class RangeHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        f, start_range, end_range = self.send_head()
+#        print "Got values of ", start_range, " and ", end_range, "...\n"
         if f:
-            self.copyfile(f, self.wfile)
+            f.seek(start_range, 0)
+            chunk = 0x1000
+            total = 0
+            while chunk > 0:
+                if start_range + chunk > end_range:
+                    chunk = end_range - start_range
+                try:
+                    self.wfile.write(f.read(chunk))
+                except:
+                    break
+                total += chunk
+                start_range += chunk
             f.close()
-        logging.error('xxxxxxxxxxxxxxxxxxx')
 
     def do_HEAD(self):
-        logging.error('do_HEAD,xxxxxxxxx')
-        logging.error(self.headers)
-        f = self.send_head()
+        f, start_range, end_range = self.send_head()
         if f:
             f.close()
 
-    def send_head(self, path):
+    def send_head(self):
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            if not self.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(301)
+                self.send_header("Location", self.path + "/")
+                self.end_headers()
+                return (None, 0, 0)
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
         try:
             f = open(path, 'rb')
         except IOError:
             self.send_error(404, "File not found")
-            return None
-        self.send_response(200)
-        self.send_header("Content-type", 'application/octet-stream')
+            return (None, 0, 0)
+        if "Range" in self.headers:
+            self.send_response(206)
+        else:
+            self.send_response(200)
+        self.send_header("Content-type", ctype)
         fs = os.fstat(f.fileno())
-        self.send_header("Content-Length", str(fs[6]))
+        size = int(fs[6])
+        start_range = 0
+        end_range = size
+        self.send_header("Accept-Ranges", "bytes")
+        if "Range" in self.headers:
+            s, e = self.headers['range'][6:].split('-', 1)
+            sl = len(s)
+            el = len(e)
+            if sl > 0:
+                start_range = int(s)
+                if el > 0:
+                    end_range = int(e) + 1
+            elif el > 0:
+                ei = int(e)
+                if ei < size:
+                    start_range = size - ei
+        self.send_header("Content-Range", 'bytes ' + str(start_range) + '-' + str(end_range - 1) + '/' + str(size))
+        self.send_header("Content-Length", end_range - start_range)
         self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
         self.end_headers()
-        return f
-    def copyfile(self, source, outputfile):
-        while True:
-            s = source.read(1024)
-            if not s:
-                break
-            outputfile.write(s)
-#        shutil.copyfileobj(source, outputfile)
+#        print "Sending Bytes ",start_range, " to ", end_range, "...\n"
+        return (f, start_range, end_range)
 
-class ThreadingHTTPServer(SocketServer.ThreadingMixIn,HTTPServer):
+    def list_directory(self, path):
+        try:
+            list = os.listdir(path)
+        except os.error:
+            self.send_error(404, "No permission to list directory")
+            return None
+        list.sort(key=lambda a: a.lower())
+        f = StringIO()
+        displaypath = cgi.escape(urllib.unquote(self.path))
+        f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
+        f.write("<html>\n<title>Directory listing for %s</title>\n" % displaypath)
+        f.write("<body>\n<h2>Directory listing for %s</h2>\n" % displaypath)
+        f.write("<hr>\n<ul>\n")
+        for name in list:
+            fullname = os.path.join(path, name)
+            displayname = linkname = name
+            # Append / for directories or @ for symbolic links
+            if os.path.isdir(fullname):
+                displayname = name + "/"
+                linkname = name + "/"
+            if os.path.islink(fullname):
+                displayname = name + "@"
+                # Note: a link to a directory displays with @ and links with /
+            f.write('<li><a href="%s">%s</a>\n'
+                    % (urllib.quote(linkname), cgi.escape(displayname)))
+        f.write("</ul>\n<hr>\n</body>\n</html>\n")
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        return (f, 0, length)
+
+    def translate_path(self, path):
+        # abandon query parameters
+        path = path.split('?',1)[0]
+        path = path.split('#',1)[0]
+        path = posixpath.normpath(urllib.unquote(path))
+        words = path.split('/')
+        words = filter(None, words)
+        path = os.getcwd()
+        for word in words:
+            drive, word = os.path.splitdrive(word)
+            head, word = os.path.split(word)
+            if word in (os.curdir, os.pardir): continue
+            path = os.path.join(path, word)
+        return path
+
+    def copyfile(self, source, outputfile):
+        shutil.copyfileobj(source, outputfile)
+
+    def guess_type(self, path):
+        base, ext = posixpath.splitext(path)
+        if ext in self.extensions_map:
+            return self.extensions_map[ext]
+        ext = ext.lower()
+        if ext in self.extensions_map:
+            return self.extensions_map[ext]
+        else:
+            return self.extensions_map['']
+
+    if not mimetypes.inited:
+        mimetypes.init() # try to read system mime.types
+    extensions_map = mimetypes.types_map.copy()
+    extensions_map.update({
+        '': 'application/octet-stream', # Default
+        '.py': 'text/plain',
+        '.c': 'text/plain',
+        '.h': 'text/plain',
+        '.mp4': 'video/mp4',
+        '.ogg': 'video/ogg',
+        })
+
+class ThreadingHTTPServer(SocketServer.ThreadingMixIn,BaseHTTPServer.HTTPServer):
     pass
 
 def test():
@@ -60,9 +174,12 @@ def serve_on(handler, host, port = 80):
     serveraddr = (host,port)
     server = ThreadingHTTPServer(serveraddr, handler)
     server.serve_forever()
+HOST = '127.0.0.1'
+PORT = 9854
 def main():
-#    t = threading.Timer(3.0, test)
-#    t.start()
-    serve_on(StreamHandler, HOST, PORT)
+    import threading
+    t = threading.Timer(3.0, test)
+    t.start()
+    serve_on(RangeHTTPRequestHandler, HOST, PORT)
 if __name__=='__main__':
     main()
