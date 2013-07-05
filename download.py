8a1eaa78a10e3572b0ac29971cc714c5cb8b9bc2
wujianguo <lsjustin89@gmail.com>
2013-07-05 22:57:20 +0800

    download

---
+++ download.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import urllib
+
+def download(downurl):
+	params={'url':downurl}
+	params=urllib.urlencode(params)
+	r = requests.post('http://proxy.wujianguo.org', data=params, stream=True)
+	f=open(downurl.split('/')[-1], 'wb')
+	while True:
+		s=r.raw.read(32*1024)
+		if not s:
+			break
+		f.write(s)
+	f.close()
+if __name__=="__main__":
+    download('http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2')
