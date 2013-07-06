7017173e61dc99a82e7664939a4283bc172e7d27
wujianguo <lsjustin89@gmail.com>
2013-07-06 09:36:03 +0800

    cmpdir

---
+++ cmpdirs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import filecmp
+
+
+def print_diff_files(dcmp):
+    for name in dcmp.diff_files:
+        print(name, dcmp.left, dcmp.right)
+    for sub_dcmp in dcmp.subdirs.values():
+        print_diff_files(sub_dcmp)
+
+
+def main():
+    arg_len = len(sys.argv)
+    dir1 = 'E:/temp/td/t1'
+    dir2 = 'E:/temp/td/t2'
+    if arg_len == 3:
+        dir1, dir2 = sys.argv[1:]
+        # print(dir1,dir2)
+    dcmp = filecmp.dircmp(dir1, dir2)
+    print_diff_files(dcmp)
+if __name__ == '__main__':
+    main()
